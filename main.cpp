bcc0efb3e29d460232d5edac780cddee1164cc3c
Kevin <157948577+KaijuKanan@users.noreply.github.com>
2024-11-28 23:54:25 +0000

    convertToBinary function completed

--- main.cpp
+++ main.cpp
@@ -3,9 +3,24 @@
 //Purpose:
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
+uint32_t convertToBinary(string);
+
+
 int main(){
     return 0;
+}
+
+
+uint32_t convertToBinary(string operand){
+    stringstream ss;
+    uint32_t binary;
+    ss << operand;
+    ss >> hex >> binary;
+    return binary;
 }

c02603b54e2eb237dad9faee40f72180895be527
Kevin <157948577+KaijuKanan@users.noreply.github.com>
2024-11-29 01:29:01 +0000

    bruh atlantis casino has like no fuckin places to work

--- main.cpp
+++ main.cpp
@@ -10,9 +10,12 @@
 using namespace std;
 
 uint32_t convertToBinary(string);
-void MOV(string, string, string);
+void MOV(string, string, string, string[]);
+void ADDS(string, string, string, string, string[]);
 
 int main(){
+    //initialize register array
+    string registerArray[8];
 
     return 0;
 }
@@ -27,15 +30,13 @@ uint32_t convertToBinary(string operand){
 }
 
 
-void MOV(string operation, string registerLocation, string hexValue){
+void MOV(string operation, string registerLocation, string hexValue, string registerArray[8]){
     //creaete array to represent flags
     string flagArray[4];
     //initialize flags to 0 because MOV operation doesnt affect any flags
     for(int i = 0; i < 4; i++){
         flagArray[i] = "0";
     }
-    //create array to represent registers
-    string registerArray[8];
     //remove the R part of the register location
     string rl = registerLocation.erase(0,1);
     //convert the register location to an integer
@@ -57,4 +58,27 @@ void MOV(string operation, string registerLocation, string hexValue){
     cout << endl;
     //output flags
     cout << "N: " << flagArray[0] << " Z: " << flagArray[1] << " C: " << flagArray[2] << " V: " << flagArray[3] << endl;
+}
+
+void ADDS(string operation, string result, string operand1, string operand2, string registerArray[]){
+    //creaete array to represent flags
+    string flagArray[4];
+
+    //get register location of result
+    string r = result.erase(0,1);
+    //convert the register location to an integer
+    int resultsPosition = stoi(r); 
+    //get register location of operand1
+    string op1 = operand1.erase(0,1);
+    //convert the register location to an integer
+    int operand1Position = stoi(op1);
+    //get register location of operand2
+    string op2 = operand2.erase(0,1);
+    //convert the register location to an integer
+    int operand2Position = stoi(op2);
+
+    //perform ADDS operation
+    uint32_t resultValue = convertToBinary(registerArray[operand1Position]) + convertToBinary(registerArray[operand2Position]);
+
+
 }

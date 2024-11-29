2d9983f5429565cbc8270eefb673c835ef5ce281
Kevin <157948577+KaijuKanan@users.noreply.github.com>
2024-11-29 00:20:11 +0000

    MOV function completed

--- main.cpp
+++ main.cpp
@@ -10,9 +10,10 @@
 using namespace std;
 
 uint32_t convertToBinary(string);
-
+void MOV(string, string, string);
 
 int main(){
+
     return 0;
 }
 
@@ -23,4 +24,23 @@ uint32_t convertToBinary(string operand){
     ss << operand;
     ss >> hex >> binary;
     return binary;
+}
+
+
+void MOV(string operation, string registerLocation, string hexValue){
+    //create array to represent registers
+    string registerArray[8];
+    //remove the R part of the register location
+    string rl = registerLocation.erase(0,1);
+    //convert the register location to an integer
+    int position = stoi(rl);
+    //inserting the hex value into the register array at the correct position, and making every other position 0x0
+    for(int i = 0; i < 8; i++){
+        if(i == position){
+            registerArray[i] = hexValue;
+        }
+        else{
+            registerArray[i] = "0x0";
+        }
+    }
 }
